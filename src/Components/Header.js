e68e5af047c7891f6831dd3b61baffd4a692aa82
omsatam <62635289+omsatam@users.noreply.github.com>
2021-05-21 20:21:00 +0530

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -23,7 +23,7 @@ function Header() {
     <div className="header">
       <Link to="/">
         <img
-          className="header__icon"
+          className="header__logo"
           src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png"
           alt="Airbnb"
         />
